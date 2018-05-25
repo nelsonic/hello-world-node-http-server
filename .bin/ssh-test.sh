6aa35af7f1963cfde15ecd8fdd98aabedde8c54f
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-25 23:14:19 +0100

    update secure env var to SSHPASS

---
+++ .bin/ssh-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CWD="$PWD/.bin"
+echo "CWD $CWD"
+ISSUE=$(sh $CWD/issue.sh)
+echo "ISSUE $ISSUE"
+BRANCH=$(sh $CWD/branch.sh)
+echo "BRANCH $BRANCH"
+IP="138.68.163.126"
+USER="root"
+SSH="ssh $USER@$IP"
+
+CMD="touch $BRANCH"
+echo "CMD $CMD"
+$SSH $CMD
