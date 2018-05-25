1c3c1b2a7f43fec939bd916c5850178d1b136c55
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-25 13:59:31 +0100

    got full deploy automated! :tada:

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -4,9 +4,23 @@ echo "CWD $CWD"
 ISSUE=$(sh $CWD/issue.sh)
 echo "ISSUE $ISSUE"
 BRANCH=$(sh $CWD/branch.sh)
+echo "BRANCH $BRANCH"
 IP="138.68.163.126"
-echo "ISSUE $ISSUE"
-CMD="dokku apps:create $ISSUE"
-echo "CMD $CMD"
-ssh root@138.68.163.126 $CMD # create the dokku App
-$(sh $CWD/remote.sh)         # Set Git Remote URL
+USER="root"
+SSH="ssh $USER@$IP"
+
+CREATE="dokku apps:create $ISSUE"
+echo "CREATE $CREATE"
+$SSH $CREATE             # create the dokku App
+
+$(sh $CWD/remote.sh &> /dev/null) # Set Git Remote URL
+
+PUSH="git push dokku $BRANCH:master"
+echo "PUSH $PUSH"
+$($PUSH)
+
+CERTS="sudo dokku certs:add $ISSUE < /etc/letsencrypt/live/ademo.app/certs.tar"
+$SSH $CERTS
+
+RELOAD="nginx -t && nginx -s reload &> /dev/null"
+$SSH $RELOAD
