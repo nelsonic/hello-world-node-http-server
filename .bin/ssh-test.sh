f6db4e4d3fd5b9c1e68ba39665de12a473c639be
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-26 06:44:17 +0100

    simplify .bin/ssh-test.sh example for StackOverflow quesion https://github.com/dwyl/learn-travis/issues/42#issuecomment-392257518

--- .bin/ssh-test.sh
+++ .bin/ssh-test.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-CWD="$PWD/.bin"
-echo "CWD $CWD"
-ISSUE=$(sh $CWD/issue.sh)
-echo "ISSUE $ISSUE"
-BRANCH=$(sh $CWD/branch.sh)
-echo "BRANCH $BRANCH"
-IP="138.68.163.126"
-USER="root"
-
-PASS=$(which sshpass)
-# echo "$PASS"
-# NOCHECK="-o StrictHostKeyChecking=no"
-SSH="$PASS -e ssh -i ./deploy_key $USER@$IP"
-echo "SSH $SSH"
-# CMD="touch $BRANCH"
-CMD="pwd; ~."
-echo "CMD $CMD"
-$SSH "$CMD"
-exit
+# sshpass -e uses the $SSHPASS Environment Variable:
+sshpass -e ssh -i ./deploy_key root@138.68.163.126 pwd
