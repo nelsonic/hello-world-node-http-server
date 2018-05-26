8010fe92299739fec6561aa69fd22653515f0be9
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-27 00:20:03 +0100

    uncomment issue debug lines

--- .bin/issue.sh
+++ .bin/issue.sh
@@ -2,6 +2,6 @@
 # This Bash Script has/does One Job: Return the GitHub Issue Number of the Active Branch.
 [ $TRAVIS_PULL_REQUEST_BRANCH ] && B=$TRAVIS_PULL_REQUEST_BRANCH || B=$(git rev-parse --abbrev-ref HEAD)
 IFS=# read BRANCH ISSUE <<< "$B";
-# echo "issue >> $ISSUE";
-# echo "name >> $BRANCH";
+echo "issue >> $ISSUE";
+echo "name >> $BRANCH";
 echo $(($ISSUE + 0)) # typecast to int
