c22cc06124192f8154ba5f2e8aa448540df98f2f
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-27 09:53:54 +0100

    add git config --global push.default simple to deploy.sh for https://github.com/dwyl/learn-devops/issues/33#issuecomment-392311864

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -23,6 +23,7 @@ $(git remote add $REMOTE)
 
 echo "REMOTE $(git config --get remote.dokku.url)"
 $(git fetch --unshallow) # https://github.com/dwyl/learn-devops/issues/33
+$(git config --global push.default simple)
 PUSH="git push dokku master"
 echo "PUSH $PUSH"
 $($PUSH)
