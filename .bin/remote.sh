97fa91b9afe2340c4facf947c070331f30a13a7a
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-27 01:07:13 +0100

    $(git remote add "$REMOTE"

--- .bin/remote.sh
+++ .bin/remote.sh
@@ -5,6 +5,7 @@ ISSUE=$(sh $CWD/issue.sh)
 IP="138.68.163.126"
 URL="$IP:$ISSUE"
 REMOTE="dokku dokku@$URL"
-# echo "REMOTE $REMOTE"
-[ $(git config --get remote.dokku.url) ] && $(git remote set-url $REMOTE) || $(git remote add "$REMOTE" &> /dev/null )
+echo "REMOTE $REMOTE"
+# [ $(git config --get remote.dokku.url) ] && $(git remote set-url $REMOTE) || $(git remote add "$REMOTE" &> /dev/null )
 # &> /dev/null
+$(git remote add "$REMOTE")
