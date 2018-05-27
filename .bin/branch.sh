d390336ebfb29048a97ff99c72ac976dd6e72ab0
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-27 01:01:59 +0100

    echo $TRAVIS_BRANCH as per: https://docs.travis-ci.com/user/environment-variables/#Convenience-Variables

--- .bin/branch.sh
+++ .bin/branch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This Bash Script has/does One Job: Return the name of the active Git branch.
 # echo "BRANCH $(git rev-parse --abbrev-ref HEAD)"
+echo "TRAVIS_BRANCH $TRAVIS_BRANCH"
 [ $TRAVIS_PULL_REQUEST_BRANCH ] && B=$TRAVIS_PULL_REQUEST_BRANCH || B=$(git rev-parse --abbrev-ref HEAD)
 echo $B
 # echo $TRAVIS_PULL_REQUEST_BRANCH
