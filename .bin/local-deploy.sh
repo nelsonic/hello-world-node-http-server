ae112ef4708e307b7a990bfdcc4d3e00d5222156
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-28 17:31:06 +0100

    echo git rev-parse --verify HEAD

--- .bin/local-deploy.sh
+++ .bin/local-deploy.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
+# First DELETE the dokku git remote (before re-creating it below):
+$(git remote rm dokku)
 CWD="$PWD/.bin"
-echo "CWD $CWD"
-ISSUE=$(sh $CWD/issue.sh)
-echo "ISSUE $ISSUE"
-BRANCH=$(sh $CWD/branch.sh)
-echo "BRANCH $BRANCH"
-echo "TRAVIS_BRANCH $TRAVIS_BRANCH"
-
-IP="138.68.163.126"
-USER="root"
-SSH="ssh -i ./deploy_key $USER@$IP"
-URL="$IP:$ISSUE"
-
-CREATE="dokku apps:create $ISSUE"
-echo "CREATE $CREATE"
-$SSH $CREATE             # create the dokku App
-
-# DELETE the dokku git remote (before re-creating it below):
-git remote rm dokku
-
-# set git remote:
-REMOTE="dokku dokku@$URL"
-echo "REMOTE $REMOTE"
-$(git remote add $REMOTE)
-
-echo "REMOTE $(git config --get remote.dokku.url)"
-PUSH="git push dokku $BRANCH:master"
-echo "PUSH $PUSH"
-$($PUSH)
-
-CERTS="sudo dokku certs:add $ISSUE < /etc/letsencrypt/live/ademo.app/certs.tar"
-$SSH $CERTS
-
-RELOAD="nginx -t && nginx -s reload"
-$SSH $RELOAD 
+$(sh $CWD/deploy.sh)
