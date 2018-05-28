c68a304980144c566ece7de4cf3b4c5ad180b66c
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-28 13:46:59 +0100

    echo "latestPRCommit=$latestPRCommit"

--- .bin/pr-hack.sh
+++ .bin/pr-hack.sh
@@ -7,6 +7,7 @@ if [[ "${TRAVIS_EVENT_TYPE}" == "pull_request" ]]; then
     repo_url=$(git config --get remote.origin.url)
     latestPRCommit=(${TRAVIS_COMMIT_RANGE//\.\.\./ })
     latestPRCommit=${latestPRCommit[1]}
+    echo "latestPRCommit=$latestPRCommit"
     git clone ${repo_url} ${latestPRCommit}
     cd ${latestPRCommit}
     SOURCE_BRANCH=($(git branch -r --contains ${latestPRCommit}))
