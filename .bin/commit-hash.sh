9e2a5d2dc56351f5510e938371e6eef7d28186c8
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-28 15:01:43 +0100

    tidy up deploy scripts and add comments

--- .bin/commit-hash.sh
+++ .bin/commit-hash.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 latestPRCommit=(${TRAVIS_COMMIT_RANGE//\.\.\./ })
-latestPRCommit=${latestPRCommit[1]}
-echo $latestPRCommit
-
-# via https://github.com/travis-ci/travis-ci/issues/6652#issuecomment-304426927
+echo latestPRCommit[1]
--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 CWD="$PWD/.bin"
 echo "CWD $CWD"
-ISSUE=$(sh $CWD/issue.sh)
-echo "ISSUE $ISSUE"
-BRANCH=$(sh $CWD/branch.sh)
-echo "BRANCH $BRANCH"
-echo "TRAVIS_BRANCH $TRAVIS_BRANCH"
+$DOKKU_APP=$(sh $CWD/issue.sh)
+echo "ISSUE $DOKKU_APP"
 COMMIT_HASH=$(sh $CWD/commit-hash.sh)
-
 IP="138.68.163.126"
 USER="root"
 SSH="ssh -i ./deploy_key $USER@$IP"
-URL="$IP:$ISSUE"
+URL="$IP:$DOKKU_APP"
 
-CREATE="dokku apps:create $ISSUE"
+# create the dokku App
+CREATE="dokku apps:create $DOKKU_APP"
 echo "CREATE $CREATE"
-$SSH $CREATE             # create the dokku App
+$SSH $CREATE
 
-# set git remote:
+# set git remote for the "review" dokku app:
 REMOTE="dokku dokku@$URL"
 echo "REMOTE $REMOTE"
 $(git remote add $REMOTE)
 
-echo "REMOTE $(git config --get remote.dokku.url)"
-$(git fetch --unshallow) # https://github.com/dwyl/learn-devops/issues/33
+# Travis does a "shallow" git clone so we need to "unshallow" it:
+$(git fetch --unshallow) # see: https://github.com/dwyl/learn-devops/issues/33
 $(git config --global push.default simple)
-# $(git pull origin $BRANCH)
 PUSH="git push dokku $COMMIT_HASH:refs/heads/master" # branch is always master ...?
 echo "PUSH $PUSH"
 $($PUSH)
 
-CERTS="sudo dokku certs:add $ISSUE < /etc/letsencrypt/live/ademo.app/certs.tar"
+# Apply the Letsencrypt Wildcard SSL/TLS Certificate to the app
+CERTS="sudo dokku certs:add $DOKKU_APP < /etc/letsencrypt/live/ademo.app/certs.tar"
 $SSH $CERTS
 
-RELOAD="nginx -t && nginx -s reload"
+# Reload (restart) nginx so the new app is served:
+RELOAD="nginx -t && nginx -s reload &> /dev/null"
 $SSH $RELOAD
