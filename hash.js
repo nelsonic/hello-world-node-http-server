4c613432f8962164fbefed97579388fc81ce1b10
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-29 23:25:00 +0100

    switch back to http-server temporarily

--- hash.js
+++ hash.js
@@ -6,11 +6,11 @@ const { EOL } = require('os');
 // https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
 module.exports = function git_hash (callback) {
   const filepath = resolve(__dirname, '.bin/commit-hash.sh');
-  console.log(process.env.GIT_REV);
   if (process.env.TRAVIS) { // see: https://git.io/vh3M7 (yes, it's lame!)
     return callback(null, process.env.TRAVIS_COMMIT, null);
   }
   if (process.env.GIT_REV) { // dokku app has thi in /home/dokku/<app-name>/ENV
+    console.log('process.env.GIT_REV', process.env.GIT_REV);
     return callback(null, process.env.GIT_REV, null);
   }
   return execFile('sh', [filepath], function(e, stdout, stderr) {
