e1a27dc5e712acd7e1919f285ed5b55d53322f3a
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-29 22:29:25 +0100

    use process.env.GIT_REV on dokku

--- hash.js
+++ hash.js
@@ -5,10 +5,12 @@ const { EOL } = require('os');
 // https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
 module.exports = function git_hash (callback) {
   const filepath = resolve(__dirname, '.bin/commit-hash.sh');
-  // console.log(filepath);
 
   if (process.env.TRAVIS) { // see: https://git.io/vh3M7 (yes, it's lame!)
     return callback(null, process.env.TRAVIS_COMMIT, null);
+  }
+  if (process.env.GIT_REV) { // dokku app has thi in /home/dokku/<app-name>/ENV
+    return process.env.GIT_REV;
   }
   return execFile('sh', [filepath], function(e, stdout, stderr) {
     return callback(e, stdout.replace(EOL, ''), stderr);
