bd88656fb7a65e4ac353bb6acd5a7f27ea020326
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-29 22:47:34 +0100

    return process.env.GIT_REV on an app deployed by Dokku

--- hash.js
+++ hash.js
@@ -11,7 +11,7 @@ module.exports = function git_hash (callback) {
     return callback(null, process.env.TRAVIS_COMMIT, null);
   }
   if (process.env.GIT_REV) { // dokku app has thi in /home/dokku/<app-name>/ENV
-    return process.env.GIT_REV;
+    return callback(null, process.env.GIT_REV, null);
   }
   return execFile('sh', [filepath], function(e, stdout, stderr) {
     return callback(e, stdout.replace(EOL, ''), stderr);
