2fc0908ed55b2ebd654b863b6ca9dce4c3b1c86b
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-29 20:41:23 +0100

    debug git commit hash on Trvis-CI #7

--- test.js
+++ test.js
@@ -7,11 +7,14 @@ function exec_sync (cmd) {
   return cp(cmd, { encoding: 'utf8' });
 };
 var cmd = 'git rev-parse HEAD';         // create name.zip from cwd
+console.log('cmd:', cmd)
 var hash = exec_sync(cmd);              // execute command synchronously
 // console.log(hash);
 
 assert(true !== false, "there are you happy Travis!?!");
 get_hash(function(e, GIT_COMMIT_HASH, stder) {
+  console.log(e, GIT_COMMIT_HASH, stder)
   console.log('actual:', hash, 'expected:', GIT_COMMIT_HASH);
+
   assert(hash === GIT_COMMIT_HASH, "check git commit hash for #7");
 })
