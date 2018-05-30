4fbcb553fc7b5e93e591d71b8441d0f13d49c76e
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-31 00:19:13 +0100

    remove branch.sh from root directory (still in .bin)

--- branch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This Bash Script has/does One Job: Return the name of the active Git branch.
-TRAVIS_PULL_REQUEST_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo $TRAVIS_PULL_REQUEST_BRANCH
