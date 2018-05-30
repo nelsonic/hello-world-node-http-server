9d182b1116378192e9ff82264588b996e9e11021
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-31 00:20:06 +0100

    remove issues.sh from root dir

--- issue.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This Bash Script has/does One Job: Return the name of the active Git branch.
-TRAVIS_PULL_REQUEST_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-IFS=# read BRANCH ISSUE <<< "$TRAVIS_PULL_REQUEST_BRANCH";
-# echo "issue >> $ISSUE";
-# echo "name >> $BRANCH";
-echo $(($ISSUE + 0)) # typecast to int
